5d33f523c5fa649d7aa53b47478936f40b8239de
spohrq <spohrq@K129-13.dep-info.iutmontp.univ-montp2.fr>
2021-03-04 11:10:09 +0100

    premier commit

---
+++ src/App/Catalogue.java
@@ -0,0 +1,46 @@
+package App;
+
+public class Catalogue {
+
+	public Catalogue() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	/*
+	public bool addProduit(In p:I_Produit) {
+		
+	}
+	
+	public bool addProduit(In nom:string, In prixHT:real, In qte:integer) {
+	
+	}
+	
+	public integer addProduits(In l:List<l_Produit>) {
+	
+	}
+	
+	public bool removeProduit(In nom:string) {
+	
+	}
+	
+	public bool acheterStock(In nom:string, In qte:integer) {
+	
+	}
+	
+	public bool vendreStock(In nom:string, In qte:integer) {
+	
+	}
+	
+	public void getNomProduits():String[] {
+	
+	}
+	
+	public void getMontantTotalTTC():real {
+	
+	}
+	
+	public void toString():string {
+	
+	}
+	*/
+}
---
+++ src/App/Produit.java
@@ -0,0 +1,52 @@
+package App;
+
+public class Produit {
+
+	/*
+	private integer quantiteStock
+	
+	private void nom:string
+	
+	private void prixUnitaireHT:real
+	
+	private void tauxTVA:real=0.2
+	*/
+	
+	public Produit(String nom, double prixHT, int quantite) {
+		// TODO Auto-generated constructor stub
+	}
+
+	/*
+	public bool ajouter(In qteAcheter:integer) {
+	
+	}
+	
+	public bool enlever(In qteVendue:integer) {
+	
+	}
+	
+	public void getPrixUnitaireHT():real {
+	
+	}
+	
+	public void getPrixUnitaireTTC():real {
+	
+	}
+	
+	public void getPrixStockTTC():real {
+	
+	}
+	
+	public void getNom():string {
+	
+	}
+	
+	public integer getQuantite() {
+	
+	}
+	
+	public void toString():string {
+	
+	}
+	*/
+}
