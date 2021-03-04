e6fe3a82e8a4827e6d44f828f7adf69be3588287
David-Durant <80038006+David-Durant@users.noreply.github.com>
2021-03-04 11:54:30 +0100

    Update Produit.java

--- src/App/Produit.java
+++ src/App/Produit.java
@@ -2,60 +2,63 @@ package App;
 
 public class Produit implements I_Produit {
 
-	/*
-	private integer quantiteStock
+
+	private int quantiteStock;
 	
-	private void nom:string
+	private String nom;
 	
-	private void prixUnitaireHT:real
+	private double prixUnitaireHT;
 	
-	private void tauxTVA:real=0.2
-	*/
+	private double tauxTVA = 0.2;
+
+	private double prixUnitaireTTC;
+	
+	private double prixStockTTC;
 	
 	public Produit(String nom, double prixHT, int quantite) {
-		// TODO Auto-generated constructor stub
+		this.nom = nom;
+		prixUnitaireHT = prixHT;
+		quantiteStock = quantite;
 	}
 
 	@Override
 	public boolean ajouter(int qteAchetee) {
-		// TODO Auto-generated method stub
+		quantiteStock = quantiteStock + qteAchetee;
 		return false;
 	}
 
 	@Override
 	public boolean enlever(int qteVendue) {
-		// TODO Auto-generated method stub
+		quantiteStock = quantiteStock - qteVendue;
 		return false;
 	}
 
 	@Override
-	public String getNom() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public int getQuantite() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
 	public double getPrixUnitaireHT() {
-		// TODO Auto-generated method stub
-		return 0;
+		return prixUnitaireHT;
 	}
 
 	@Override
 	public double getPrixUnitaireTTC() {
-		// TODO Auto-generated method stub
-		return 0;
+		prixUnitaireTTC = prixUnitaireHT * (1+tauxTVA);
+		return prixUnitaireTTC;
 	}
 
 	@Override
 	public double getPrixStockTTC() {
-		// TODO Auto-generated method stub
-		return 0;
+		prixStockTTC = getPrixUnitaireTTC() * quantiteStock;
+		return prixStockTTC;
+	}
+	
+	@Override
+	public String getNom() {
+		return nom;
 	}
 
+	@Override
+	public int getQuantite() {
+		return quantiteStock;
+	}
+
+	
 }
